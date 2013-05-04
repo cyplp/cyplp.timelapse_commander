ff2275702e6a82769fb0dba68ab382be4efb970c
Cyprien Le Pannérer <cyplp@free.fr>
2013-05-04 18:01:51 +0200

    Add basic pages

--- cyplp/timelapse_commander/__init__.py
+++ cyplp/timelapse_commander/__init__.py
@@ -5,7 +5,11 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('home', '/')
+    config.add_route('controls', '/controls')
+    config.add_route('batchs', '/batchs')
+
     config.include('rebecca.fanstatic')
     config.add_fanstatic_resources(['js.bootstrap.bootstrap'], r'.*\.pt')
 
--- cyplp/timelapse_commander/views.py
+++ cyplp/timelapse_commander/views.py
@@ -1,5 +1,13 @@
 from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.pt')
-def my_view(request):
+def home(request):
     return {'project':'cyplp.timelapse_commander'}
+
+@view_config(route_name='controls', renderer='templates/controls.pt')
+def controls(request):
+    return {}
+
+@view_config(route_name='batchs', renderer='templates/batchs.pt')
+def batchs(request):
+    return {}
